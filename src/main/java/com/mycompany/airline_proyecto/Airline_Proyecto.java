692472d2263636ff81f2080ebb353691991e1d52
MiltonDavila2 <miltondaviladt@gmail.com>
2025-01-14 10:02:40 -0500

    hola

---
+++ src/main/java/com/mycompany/airline_proyecto/Airline_Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airline_proyecto;
+
+/**
+ *
+ * @author Milton Davila
+ */
+public class Airline_Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
